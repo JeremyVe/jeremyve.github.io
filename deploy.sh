0cdf296d86dacfbe491d51262cd4ca2929666eaf
JeremyVe <jepschx@gmail.com>
2021-06-26 15:34:56 +0200

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git remote add origin https://github.com/JeremyVe/jeremyve.github.io.git
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f origin master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd ..
